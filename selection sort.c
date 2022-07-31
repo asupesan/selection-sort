0a48f8e1a39f7ad82d5eddaf90057e102f2f88cd
asupesan <63061932+asupesan@users.noreply.github.com>
2022-07-31 17:46:41 -0300

    Update selection sort.c

--- selection sort.c
+++ selection sort.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define t 100
 
-int preencheC(int v[]){
+int preencheC(int v[]){ // preenche de forma crescente
   for(int i = 0; i < t; i++){
     v[i] = i+1;
   }
   return 0;
 }
 
-int preencheD(int v[]){
+int preencheD(int v[]){ // preenche de forma decrescente
   for(int i = 0; i < t; i++){
     v[i] = t - i;
   }
   return 0;
 }
 
+int uniqueR(int v[], int i){ // faz com que os numeros da funçao rand sejam unicos
+  for(int j = 0; j < i; j++){ // obrigando a repetir o codigo 
+    if(v[j] == v[i]) // até q saia um número q n esteja contido
+      return 0;
+  }
+  return 1;
+}
+
 int preencheR(int v[]){
   for(int i = 0; i < t; i++){
     v[i] = rand() % t;
+    if(uniqueR(v, i) == 0)
+      i--; // faz com que a posiçao se repita até que a funçao uniqueR retorne 1
   }
   return 0;
 }
@@ -30,7 +41,10 @@ int print(int v[]){
   return 0;
 }
 
-int ss(int v[]){
+double ss(int v[]){
+  double time_spent = 0.0;
+  clock_t begin = clock();
+  
   int i = t-1, min = v[i], hp = i, b = 0;
   while(b != t){ // condiçao de parada
     for(i = t-1; i >= b; i--){ // navegar no vetor
@@ -46,18 +60,23 @@ int ss(int v[]){
     b++; // incrementa o criterio de parada e sobe o valor da posicáo considerada inferior
   // print(v);
   }
-  return 0;
+  
+  clock_t end = clock();
+  
+  
+  return time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
 int main(void){
   int v[t];
-
+  double time;
   preencheR(v);
   printf("Antes de ordenar:\n");
   print(v);
-  ss(v);
+  time = ss(v);
   printf("Depois de ordenar:\n");
   print(v);
-    
-    return 0;
+  printf("Tempo: %f\n", time); 
+  
+  return 0;
 }
